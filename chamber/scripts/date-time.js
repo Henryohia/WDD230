9de4beb5a9126d049349a4020a838af17482ec30
Henry Ohia <Henry Ohia>
2023-11-25 14:19:27 +0100

    Modified join page

--- chamber/scripts/date-time.js
+++ chamber/scripts/date-time.js
@@ -5,11 +5,11 @@ var d = new Date();
 // Set the value of the "date" field
 document.getElementById("date").value = d.toDateString();
 
-// Set the value of the "time" field
-var hours = d.getHours();
-var mins = d.getMinutes();
-var seconds = d.getSeconds();
-document.getElementById("time").value = hours + ":" + mins + ":" + seconds;
+// // Set the value of the "time" field
+// var hours = d.getHours();
+// var mins = d.getMinutes();
+// var seconds = d.getSeconds();
+// document.getElementById("time").value = hours + ":" + mins + ":" + seconds;
 
 // Get today's date
 var day = d.getDate();
